2e2fbe62b1481ea5d6e64f01c1da0dfd18bedaa9
Philip Hughes <p@hews.co>
2020-04-30 13:12:15 -0700

    reformat and update JS for sending credentials verify

--- admin/js/akamai-admin.js
+++ admin/js/akamai-admin.js
@@ -1,83 +1,73 @@
 (function( $ ) {
-	'use strict';
+    'use strict';
 
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+    function getCredentials() {
+        return {
+            'host': $('#akamai-credentials-host').val(),
+            'access-token': $('#akamai-credentials-access-token').val(),
+            'client-token': $('#akamai-credentials-client-token').val(),
+            'client-secret': $('#akamai-credentials-client-secret').val(),
+        };
+    }
 
-	$(function() {
+    function setVerifyButtonDisabled() {
+        var creds = getCredentials();
+        var vals = Object.keys(creds).map(function(key) {
+            return creds[key];
+        });
+        $('#verify').prop('disabled', vals.includes(''));
+    }
+
+    $(function() {
+        setVerifyButtonDisabled();
+        $("form :input").keyup(setVerifyButtonDisabled);
+        $("form :input").change(setVerifyButtonDisabled);
         $('#verify').click(function(e) {
             e.stopPropagation();
 
-			var data = {
-				'action': 'akamai_verify_credentials',
-				'edgerc': $('#akamai-edgerc').val(),
-				'section': $('#akamai-section').val()
-			};
+            var data = {
+                'action': 'akamai_verify_credentials',
+                'credentials': getCredentials(),
+            };
 
-			// We can also pass the url value separately from ajaxurl for front end AJAX implementations
-			$.post(ajaxurl, data, function(response) {
-				var response = $.parseJSON(response);
-				var timeout = false;
+            // We can also pass the url value separately from ajaxurl for front end AJAX implementations
+            $.post(ajaxurl, data, function(response) {
+                var response = $.parseJSON(response);
+                var timeout = false;
 
-				if ($('#verify-msg').length == 0) {
-					$('#verify').before($('<div id="verify-msg" class="notice"></div>'));
-				} else {
-					$('#verify-msg').empty();
-				}
+                if ($('#verify-msg').length == 0) {
+                    $('#verify').before($('<div id="verify-msg" class="notice"></div>'));
+                } else {
+                    $('#verify-msg').empty();
+                }
 
-				if (timeout) {
-					clearInterval(timeout);
-				}
+                if (timeout) {
+                    clearInterval(timeout);
+                }
 
-				var msg = $('#verify-msg');
+                var msg = $('#verify-msg');
 
-				timeout = setTimeout(function() {
-					msg.fadeOut();
-					msg.remove();
-				}, 5000);
+                timeout = setTimeout(function() {
+                    msg.fadeOut();
+                    msg.remove();
+                }, 5000);
 
-				if (response.success) {
-					msg.removeClass('notice-error');
-					msg.addClass('notice-success');
-					msg.append($('<p>Credentials found successfully.</p>'));
-				} else if (response.error) {
-					msg.removeClass('notice-success');
-					msg.addClass('notice-error');
-					msg.append($('<p>' + response.error + '</p>'));
-				} else {
-					msg.removeClass('notice-success');
-					msg.addClass('notice-error');
-					msg.append($('<p>An unknown error occured</p>'));
-				}
+                if (response.success) {
+                    msg.removeClass('notice-error');
+                    msg.addClass('notice-success');
+                    msg.append($('<p>Credentials verified successfully.</p>'));
+                } else if (response.error) {
+                    msg.removeClass('notice-success');
+                    msg.addClass('notice-error');
+                    msg.append($('<p>' + response.error + '</p>'));
+                } else {
+                    msg.removeClass('notice-success');
+                    msg.addClass('notice-error');
+                    msg.append($('<p>An unknown error occured</p>'));
+                }
 
-				$('#verify').before(msg);
-			});
-		});
+                $('#verify').before(msg);
+            });
+        });
     });
-
 })( jQuery );
